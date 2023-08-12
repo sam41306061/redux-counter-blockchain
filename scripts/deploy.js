9c9f444e0495588c22d4d259c7815432d81c3559
Sam Schoenberg <sam4130@Sams-MacBook-Pro.local>
2023-08-12 17:25:05 -0600

    working setup for connecting wallet to the application

---
+++ scripts/deploy.js
@@ -0,0 +1,18 @@
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+
+
+async function main() {
+    
+}
+
+
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });
+  
