59c4c155d3c1099e4eaee4001241f686c1de9ff5
Sam Schoenberg <sam4130@Sams-MacBook-Pro.local>
2023-08-24 03:49:23 -0600

    final working updated createSlice structure

--- src/store/store.js
+++ src/store/store.js
@@ -1,40 +1,30 @@
-import { createStore } from 'redux';
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 
+const initialState = { counter: 0, showCounter: true}
 
-const counterReducer = (state = {counter: 0}, action) => { 
-    //  users adds something 
-    if(action.type === 'increment') {
-        return {
-            counter: state.counter + 1,
-            showCounter: state.showCounter
-        }
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState,
+    reducers: {
+        increment(state) {
+            state.counter++;
+        },
+        decrement(state) {
+            state.counter--;
+        },
+        multiply(state, action) {
+            state.counter = state.counter * parseInt(action.payload);
+        },
+        toggle(state) {
+            state.showCounter = !state.showCounter;
+        },
     }
-    // user subtracts something 
-    if(action.type === 'decrement') {
-        return {
-            counter: state.counter - 1,
-            showCounter: state.showCounter
-        }
-    }
-    // users multiply
-    if(action.type === 'multiply') {
-        return {
-            counter: state.counter * parseInt(action.payload),
-            showCounter: state.showCounter
-        }
-    }
-    if(action.type === 'toggle') {
-        return {
-            showCounter: !state.showCounter,
-            counter: state.counter
-        }
-        
-    }
+});
 
-    // returns state with no changes 
-    return state;
-};
+const store = configureStore({
+    
+    reducer: counterSlice.reducer
+});
 
-const store = createStore(counterReducer);
-
+export const counterActions = counterSlice.actions;
 export default store;
