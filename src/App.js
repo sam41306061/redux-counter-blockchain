819e42e694ea98800b5822fc0aee8b90c42c2a62
Sam Schoenberg <sam4130@Sams-MacBook-Pro.local>
2023-08-24 03:56:52 -0600

    inital setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import Counter from './components/Counter';
+import { Fragment } from 'react';
 
+// local compoents 
+import Counter from './components/Counter';
+import Header from './components/Header';
+import Auth from './components/Auth';
 
 function App() {
   return (
-    <Counter />
+    <Fragment>
+      <Header/>
+      <Auth/>
+      <Counter />
+    </Fragment>
+   
   );
 }
 
