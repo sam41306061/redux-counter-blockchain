103f478e6ff53610b2dbdf0550fadea771bb300b
Sam Schoenberg <sam4130@Sams-MacBook-Pro.local>
2023-08-27 11:27:45 -0600

    working frontend connection to meta and ether blockchain

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import { Fragment } from 'react';
+import { Fragment, useEffect } from 'react';
 import { useSelector } from 'react-redux';
+
+// remove after testing
+import { ethers } from 'ethers';
 
 // local compoents 
 import Counter from './components/Counter';
@@ -9,6 +12,18 @@ import UserProfile from './components/UserProfile'
 
 function App() {
   const isAuth = useSelector(state => state.auth.isAuthenticated)
+
+  const loadBlockChainData = async() =>  {
+    const accounts = await window.ethereum.request({method: 'eth_requestAccounts'});
+    console.log(accounts[0]);
+
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const network = await provider.getNetwork();
+    console.log(network.chainId);
+  }
+  useEffect(() => {
+    loadBlockChainData();
+  })
   return (
     <Fragment>
       <Header/>
--- src/store/store.js
+++ src/store/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import counterReducer from './counter';
 import authReducer from './auth';
+import accountsReducer from './accounts';
 
 const store = configureStore({
   reducer: {
     counter: counterReducer,
-    auth: authReducer
+    auth: authReducer,
+    accounts: accountsReducer
   },
 });
 
