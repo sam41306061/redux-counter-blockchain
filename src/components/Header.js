40c5fea910112902c206c3e7127ce1fc2adfb831
Sam Schoenberg <sam4130@Sams-MacBook-Pro.local>
2023-10-28 07:32:06 -0600

    inital hook up of account navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,8 @@ import { useSelector, useDispatch } from 'react-redux';
 import classes from './Header.module.css';
 
 // state handler
-import {authActions} from '../store/auth';
-import { loadProvider,loadNetwork,loadAccount,loadBalance } from '../store/accounts';
+import { authActions } from '../store/auth';
+import { loadAccount } from '../store/accounts';
 
 const Header = () => {
   const dispatch = useDispatch();
@@ -20,36 +20,44 @@ const Header = () => {
     })
   }
 
-
+const connectHandler = async() =>{
+  await loadAccount(provider, dispatch);
+}
 
   const logOutHandler = (event) => {
     event.preventDefault();
     dispatch(authActions.logout());
-   }
+  }
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
       {isAuth && (
         <nav>
-        <ul>
-          <li>
-            <a href='/'>Connect Account</a>
-          </li>
-          <li>
-            <p><small>{Number(balance).toFixed(4)}</small></p>
-          </li>
-          {network && (
-          <li>
-            <button herf='' value={[network] ? `0x${network.toString(16)}` : `0`} onClick={networkHandler()}>Select Network</button>
-          </li>
-          )}
-          <li>
-            <button onClick={logOutHandler}>Logout</button>
-          </li>
-        </ul>
-      </nav>
+          <ul>
+            {account ? (
+              <li>
+              <a href='/'>{account.slice(0,5) + '...' + account.slice(38,42)}</a>
+            </li>
+            ):(
+              <li>
+              <a href='/' onClick={connectHandler}>Connect Account</a>
+            </li>
+            )}
+            <li>
+              <p><small>{Number(balance).toFixed(4)}</small></p>
+            </li>
+            {network && (
+              <li>
+                <button herf='' value={[network] ? `0x${network.toString(16)}` : `0`} onClick={networkHandler()}>Select Network</button>
+              </li>
+            )}
+            <li>
+              <button onClick={logOutHandler}>Logout</button>
+            </li>
+          </ul>
+        </nav>
       )}
-      
+
     </header>
   );
 };
