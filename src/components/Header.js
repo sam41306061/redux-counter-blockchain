2134e5fabf38d4d433774516fb9d55b6b3ead6a4
Sam Schoenberg <119435214+sam41306061@users.noreply.github.com>
2023-11-11 15:05:45 -0700

    second commit -use for any front end connect

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,34 +3,59 @@ import classes from './Header.module.css';
 
 // state handler
 import {authActions} from '../store/auth';
+import { loadAccount } from '../store/accounts';
 
 const Header = () => {
   const dispatch = useDispatch();
   const isAuth = useSelector(state => state.auth.isAuthenticated);
+  const provider = useSelector(state => state.accounts.connection);
+  const network = useSelector(state => state.accounts.chainId);
+  const account = useSelector(state => state.accounts.account);
+  const balance = useSelector(state => state.accounts.balance);
 
+  const networkHandler = async (e) => {
+    await window.ethereum.request({
+      method: 'wallet_switchEthereumChain',
+      params: [{ network: e.target.value }],
+    })
+  }
+  const connectHandler = async() =>{
+    await loadAccount(provider, dispatch);
+  }
   const logOutHandler = (event) => {
     event.preventDefault();
     dispatch(authActions.logout());
    }
-  return (
+   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
       {isAuth && (
         <nav>
-        <ul>
-          <li>
-            <a href='/'>My Products</a>
-          </li>
-          <li>
-            <a href='/'>My Sales</a>
-          </li>
-          <li>
-            <button onClick={logOutHandler}>Logout</button>
-          </li>
-        </ul>
-      </nav>
+          <ul>
+            {account ? (
+              <li>
+              <a href='/'>{account.slice(0,5) + '...' + account.slice(38,42)}</a>
+            </li>
+            ):(
+              <li>
+              <a href='/' onClick={connectHandler}>Connect Account</a>
+            </li>
+            )}
+            <li>
+              <p><small>{Number(balance).toFixed(4)}</small></p>
+            </li>
+            {network && (
+              <li>
+                <button herf='' value={[network] ? `0x${network.toString(16)}` : `0`} onClick={networkHandler()}>Select Network</button>
+              </li>
+            )}
+            <li>
+              <button onClick={logOutHandler}>Logout</button>
+            </li>
+          </ul>
+        </nav>
       )}
-      
+
     </header>
   );
 };
--- src/store/accounts.js
+++ src/store/accounts.js
@@ -1,4 +1,4 @@
-import { createSlice} from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice} from "@reduxjs/toolkit";
 import { ethers } from "ethers";
 
 const initalAccountsState = {
@@ -8,16 +8,50 @@ const initalAccountsState = {
     balance: null
 }
 
+export const loadProvider = createAsyncThunk(
+    'accounts/loadProvider',
+    async() => {
+        const connection = new ethers.providers.Web3Provider(window.ethereum)
+        return connection;
+    }
+);
+
+export const loadNetwork = createAsyncThunk(
+    'accounts/loadNetwork',
+    async() => {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const network = await provider.getNetwork();
+        return network;
+    }
+)
+
+export const loadAccount = createAsyncThunk(
+    'accounts/loadAccount',
+    async() => {
+        const account = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        return account[0];
+    }
+);
+
+export const loadBalance = createAsyncThunk(
+    'accounts/loadBalance',
+    async() => {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        const account = accounts[0];
+        const balance = await provider.getBalance(account);
+        return balance;
+    }
+)
+
+
+
 const accountsSlice = createSlice({
     name: 'accounts',
     initialState: initalAccountsState,
     reducers: {
         providerLoaded(state,action) {
-            const provider = new ethers.providers.Web3Provider(action.payload.connection);
-            provider.getNetwork().then(network => {
-                state.chainId = network.chainId;
-            })
-            state.connection = action.payload.connection;
+          state.connection = action.payload.connection;
         },
         networkLoaded(state,action) {
             state.chainId = action.payload.chainId;
@@ -28,6 +62,20 @@ const accountsSlice = createSlice({
         ethersBalanceLoaded(state,action){
             state.balance = action.payload.balance;
         }
+    },
+    extraReducers: (builder) => {
+        builder.addCase(loadProvider.fulfilled, (state,action) => {
+            state.connection = action.payload
+        });
+        builder.addCase(loadNetwork.fulfilled, (state,action) =>{
+            state.chainId = action.payload
+        })
+        builder.addCase(loadAccount.fulfilled, (state,action) => { 
+            state.account = action.payload
+        });
+        builder.addCase(loadBalance.fulfilled, (state, action) =>{
+            state.balance = action.payload
+        })
     }
 });
 
